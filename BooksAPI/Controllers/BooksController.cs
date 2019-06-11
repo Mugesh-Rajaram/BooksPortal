173fb201ccc4f1ceb9e2f44bc6935730abaaa217
Mugesh <mugeshrajaram@gmail.com>
2019-06-11 11:16:19 +0530

    Controller Added

---
+++ BooksAPI/Controllers/BooksController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BooksAPI.Controllers
+{
+    [Route("api/books")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        public IActionResult GetBooks()
+        {
+
+            return Ok();
+        }
+    }
+}
